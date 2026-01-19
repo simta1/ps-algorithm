3cc607557b72c9d05a5a90a28c40bcf3e05cb952
simta1 <koreajunho0219@gmail.com>
2026-01-20 01:58:45 +0900

    feat: random, time

--- src/misc/random.cpp
+++ src/misc/random.cpp
@@ -1,2 +1,6 @@
 mt19937 rng;
-shuffle(v.begin(), v.end(), rng);
+shuffle(v.begin(), v.end(), rng); // O(N)
+
+uniform_int_distribution<int> dist(a, b); // a<=x<=b
+uniform_real_distribution<double> dist(a, b); // a<=x<b
+x = dist(rng);
---
+++ src/misc/time.cpp
@@ -0,0 +1,2 @@
+clock_t sttime = clock();
+while(double(clock() - sttime) / CLOCKS_PER_SEC < 0.95) { // code }
