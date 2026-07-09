e9b00f6a271a4473378cb5891e1b52b26aff9fdb
simta1 <koreajunho0219@gmail.com>
2026-07-10 04:39:30 +0900

    refactor: segtree

--- src/data_structure/_segtree(maximum_subarray_sum).cpp
+++ src/data_structure/_segtree(maximum_subarray_sum).cpp
@@ -1,73 +1,20 @@
-template <typename T>
-class GoldMineSeg {
-private:
-    struct Node {
-        T sum, l, r, lr;
-
-        Node() : sum(0), l(0), r(0), lr(0) {}
-        Node(T sum, T l, T r, T lr) : sum(sum), l(l), r(r), lr(lr) {}
-    };
-
-    int n;
-    vector<Node> tree;
-    const T INF;
-
-    Node merge(const Node &a, const Node &b) {
-        Node ret;
-        ret.sum = a.sum + b.sum;
-        ret.l = max(a.l, a.sum + b.l);
-        ret.r = max(b.r, a.r + b.sum);
-        ret.lr = max({a.r + b.l, a.lr, b.lr});
-        return ret;
-    }
-
-    void update(int node, int s, int e, int i, T val, bool add) {
-        if (i < s || e < i) return;
-        if (s == e) {
-            if (add) val += tree[node].sum;
-            tree[node] = Node(val, val, val, val);
-            return;
-        }
-
-        int m = s + e >> 1;
-        update(node * 2, s, m, i, val, add);
-        update(node * 2 + 1, m + 1, e, i, val, add);
-        tree[node] = merge(tree[node * 2], tree[node * 2 + 1]);
-    }
-
-    Node query(int node, int s, int e, int l, int r) {
-        if (l <= s && e <= r) return tree[node];
-        if (l > e || s > r) return Node(0, -INF, -INF, -INF);
-
-        int m = s + e >> 1;
-        Node lq = query(node * 2, s, m, l, r);
-        Node rq = query(node * 2 + 1, m + 1, e, l, r);
-        return merge(lq, rq);
-    }
-
-public:
-    GoldMineSeg(int n) : n(n), tree(4 * n), INF(numeric_limits<T>::max() >> 1) {}
-    GoldMineSeg(const vector<T> &v) : n(v.size()), tree(4 * v.size()), INF(numeric_limits<T>::max() >> 1) {
-        for (int i = 0; i < v.size(); i++) updateChange(i + 1, v[i]);
-    }
-
-    void updateAdd(int i, T val) { // 1-based
-        update(1, 0, n - 1, i - 1, val, 1);
-    }
-
-    void updateChange(int i, T val) { // 1-based
-        update(1, 0, n - 1, i - 1, val, 0);
-    }
-
-    T query(int l, int r) { // 1-based
-        return query(1, 0, n - 1, l - 1, r - 1).lr;
-    }
+struct Node {
+    ll sum, l, r, lr;
+    Node(ll x) : sum(x), l(x), r(x), lr(x) {}
+    Node(ll sum, ll l, ll r, ll lr) : sum(sum), l(l), r(r), lr(lr) {}
 };
-/*
-`query()`는 연속부분합들 중 최댓값 계산
-이때 구간길이가 최소 1인 것들 중에서만 계산
-즉, 값이 전부 음수면 답이 0이 나오는 게 아니라 음수들 중 최대값이 나옴
-
-v[1:n]이 A과 B로 이루어졌을 때 A로만 이루어진 구간의 최대 길이 구하는 법
-A -> 1, B -> -n을 val로 취급해서 최대연속합 구하면 됨
-*/
+Node op(const Node &a, const Node &b) {
+    return {
+        a.sum + b.sum,
+        max(a.l, a.sum + b.l),
+        max(b.r, a.r + b.sum),
+        max({a.lr, b.lr, a.r + b.l})
+    };
+}
+Node e() {
+    return {0, -INF, -INF, -INF};
+}
+// 비어있지 않은 연속 부분 구간 중 최대합 계산
+// 값이 전부 음수면 답이 0이 대신 음수들 중 최대값 나옴
+// v[1:n]이 A과 B로 이루어졌을 때 A로만 이루어진 구간의 최대 길이:
+// A -> 1, B -> -n으로 두면 최대 연속 부분합과 같음
--- src/data_structure/segtree.cpp
+++ src/data_structure/segtree.cpp
@@ -1,39 +1,24 @@
 template<typename T, T (*op)(const T&, const T&), T (*e)()>
-class SegmentTree {
-private:
-    int n, sz, log;
+struct Seg {
+    int n, sz;
     vector<T> tree;
-
-    static int pow2GE(int n) { assert(n); return 1 << 32 - __builtin_clz(n) - !(n & ~-n); }
-
-public:
-    SegmentTree() = default;
-    SegmentTree(int n) : SegmentTree(vector<T>(n, e())) {}
-    SegmentTree(const vector<T>& v) : n(v.size()) { // vector v는 0-based로 넘겨주지만 이후 update, query 등은 1-based로 하게 됨에 주의
-        sz = pow2GE(n);
+    Seg(int n) : Seg(vector<T>(n, e())) {}
+    Seg(const vector<T>& v) : n(v.size()) {
+        for (sz = 1; sz < n; sz <<= 1);
         tree = vector<T>(sz << 1, e());
-        for (int i = 0; i < n; i++) tree[sz + i] = v[i];
+        for (int i = 0; i < n; i++) tree[sz | i] = v[i];
         for (int i = sz - 1; i >= 1; i--) tree[i] = op(tree[i << 1], tree[i << 1 | 1]); 
     }
-
-    void updateChange(int i, T val) { // 1-based
-        assert(1 <= i && i <= n);
-        tree[--i |= sz] = val;
-        while (i >>= 1) tree[i] = op(tree[i << 1], tree[i << 1 | 1]); 
-    }
-
-    void updateAdd(int i, T val) { // 1-based
+    void update(int i, T val) { // 1-based
         assert(1 <= i && i <= n);
         --i |= sz;
         tree[i] = op(tree[i], val);
         while (i >>= 1) tree[i] = op(tree[i << 1], tree[i << 1 | 1]); 
     }
-    
     T get(int i) const { // 1-based
         assert(1 <= i && i <= n);
         return tree[--i | sz];
     }
-
     T query(int l, int r) const { // 1-based // [l:r]
         assert(1 <= l && l <= r && r <= n);
         T resL = e(), resR = e();
@@ -43,63 +28,50 @@ public:
         }
         return op(resL, resR);
     }
-    
-    int findRightMost(int l, const function<bool(T)> &f) const { // f([l:r]) = true인 최대의 r을 찾음, l과 r 둘 다 1-based // 만족하는 r이 없다면 l-1리턴
+    template <typename F>
+    int findRightMost(int l, F f) const { // f([l:r])=true인 최대 r, 1-based // 만족하는 r 없다면 l-1리턴
         assert(1 <= l && l <= n);
         int node = (l - 1) | sz;
         T acc = e();
-        
-        node >>= __builtin_ctz(node); // node[l, r]에서 l은 그대로 두고 r만 최대한 오른쪽으로 이동
+        node >>= __builtin_ctz(node);
         while (f(op(acc, tree[node]))) {
-            acc = op(acc, tree[node++]); // f(node[l, r]) 만족하므로 node+1[r+1, ...]로 이동
-            if (!(node & (node - 1))) return n; // node가 2^k꼴 -> 이전의 node-1=2^k-1[l,r=n]꼴
+            acc = op(acc, tree[node++]);
+            if (!(node & (node - 1))) return n;
             node >>= __builtin_ctz(node);
         }
-        
         while (node < sz) {
-            node <<= 1; // node[l, r] -> node*2[l, m]
-            if (f(op(acc, tree[node]))) acc = op(acc, tree[node++]); // [m + 1, r]
+            node <<= 1;
+            if (f(op(acc, tree[node]))) acc = op(acc, tree[node++]);
         }
-        return node ^ sz; // f(node)=false인 상황 -> node-1 리턴해야 해야 되서 (node - 1 - sz) -> 1-based로 (node - sz)
+        return node ^ sz; // 1-based
     }
-    
-    int findKthSmallest(int l, T k) const { // query([l, r]) >= k인 최소의 r 찾음, l과 r 둘 다 1-based // 즉, l에서부터 오른쪽으로 k번째 지점 찾는거
-        assert(k >= 0);
-        return query(l, n) < k ? -1 : findRightMost(l, [&](T sum) { return sum < k; }) + 1;
-    }
-    
-    int findLeftMost(int r, const function<bool(T)> &f) const { // f([l:r]) = true인 최소의 l을 찾음, l과 r 둘 다 1-based // 만족하는 l이 없다면 r+1리턴
+    template <typename F>
+    int findLeftMost(int r, F f) const { // f([l:r])=true인 최소 l, 1-based // 만족하는 l 없다면 r+1리턴
         assert(1 <= r && r <= n);
         int node = (r - 1) | sz;
         T acc = e();
-    
-        node = max(1, node >> __builtin_ctz(~node)); // node[l, r]에서 r은 그대로 두고 l만 최대한 왼쪽으로 이동, 최대로 이동한 게 루트여야 되므로 0이 되면 1로 바꿔줌
+        node = max(1, node >> __builtin_ctz(~node));
         while (f(op(tree[node], acc))) {
-            acc = op(tree[node--], acc);  // f(node[l, r]) 만족하므로 node-1[..., l-1]로 이동
-            if (!((node + 1) & node)) return n; // node가 2^k-1꼴 -> 이전의 node+1=2^k[l=1,r]꼴이었던 거
+            acc = op(tree[node--], acc);
+            if (!((node + 1) & node)) return 1;
             node = max(1, node >> __builtin_ctz(~node));
         }
-    
         while (node < sz) {
-            node = node << 1 | 1; // node[l, r] -> node*2+1[m+1,r]
-            if (f(op(tree[node], acc))) acc = op(tree[node--], acc); // [l, m]
+            node = node << 1 | 1;
+            if (f(op(tree[node], acc))) acc = op(tree[node--], acc);
         }
-        return node + 2 - sz; // f(node)=false인 상황 -> node+1 리턴해야 해야 되서 (node + 1 - sz) -> 1-based로 (node + 2 - sz)
+        return node + 2 - sz; // 1-based
     }
-    
-    int findKthLargest(int r, T k) const { // query([l, r]) >= k인 최대의 l 찾음, l과 r 둘 다 1-based // 즉, r에서부터 왼쪽으로 k번째 지점 찾는거
-        assert(k >= 0);
+    int findKthSmallest(int l, T k) const { // query([l, r]) >= k인 최소 r // 1-based
+        assert(k >= 1);
+        return query(l, n) < k ? -1 : findRightMost(l, [&](T sum) { return sum < k; }) + 1;
+    }
+    int findKthLargest(int r, T k) const { // query([l, r]) >= k인 최대 l // 1-based
+        assert(k >= 1);
         return query(1, r) < k ? -1 : findLeftMost(r, [&](T sum) { return sum < k; }) - 1;
     }
 };
-
-/*
-findLeftMost(l, f)은 만족하는 r 없는 경우 l-1 리턴   
-findRightMost(r, f)은 만족하는 l 없는 경우 r+1 리턴   
-findKthSmallest, findKthLargest은 만족하는 값이 없는 경우 -1 리턴   
-
 // ex) 합 세그
 int op(const int &a, const int &b) { return a + b; }
 int e() { return 0; }
-SegmentTree<int, sum, zero> seg(v);
-*/
+Seg<int, op, e> seg(v);
