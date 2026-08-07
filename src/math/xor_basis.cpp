415c1b7376ddd28acff75cd7aa1e5a0d8f66b806
simta1 <koreajunho0219@gmail.com>
2026-08-07 14:07:07 +0900

    refactor: xor basis

--- src/math/xor_basis.cpp
+++ src/math/xor_basis.cpp
@@ -1,12 +1,9 @@
-template <typename T>
-vector<T> getXorBasis(const vector<T> &v) {
-    vector<T> basis;
-    for (auto e : v) {
-        for (auto b : basis) e = min(e, e ^ b);
-        if (e) basis.push_back(e);
-    }
-    sort(basis.rbegin(), basis.rend());
-    return basis;
-}
-// T mxSum = 0;
-// for (auto b : getXorBasis(v)) mxSum = max(mxSum, mxSum ^ b);
+vector<T> basis;
+auto insert = [&](T x) {
+    for (auto e : basis) x = min(x, x ^ e);
+    if (x) basis.push_back(x);
+};
+T mxSum = 0;
+auto b = basis; // insert를 계속해야하는 경우 basis의 순서는 바뀌면 안됨
+sort(b.rbegin(), b.rend());
+for (auto e : b) mxSum = max(mxSum, mxSum ^ e);
