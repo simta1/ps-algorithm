a2c4e1d77795ca2f5917f15cf5f0e8a8db60becb
simta1 <koreajunho0219@gmail.com>
2026-08-11 13:27:37 +0900

    refactor: z

--- src/string/z.cpp
+++ src/string/z.cpp
@@ -1,8 +1,8 @@
 template <typename Container>
-vector<int> getZ(const Container &st) { // O(N)
+vector<int> get_z(const Container &st) { // O(N)
     vector<int> z(st.size());
     for (int i = 1, p = 0; i < st.size(); i++) {
-        if (i <= p + z[p] - 1) z[i] = min(p + z[p] - 1 - i + 1, z[i - p]);
+        if (i < p + z[p]) z[i] = min(p + z[p] - i, z[i - p]);
         while (i + z[i] < st.size() && st[z[i]] == st[i + z[i]]) ++z[i];
         if (p + z[p] < i + z[i]) p = i;
     }
